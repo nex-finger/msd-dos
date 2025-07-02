117ee351231c3bdb0317d0c8b65a8a7449be07c0
Mizuki Masuda <2020Tibakou@gmail.com>
2025-07-02 20:20:52 +0900

    syscall_file_upload

---
+++ msys.X/syscall.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   syscall.c
+ * Author: 2020t
+ *
+ * Created on 2025/07/02, 20:13
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ msys.X/syscall.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   syscall.h
+ * Author: 2020t
+ *
+ * Created on 2025/07/02, 20:13
+ */
+
+#ifndef SYSCALL_H
+#define	SYSCALL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYSCALL_H */
+
